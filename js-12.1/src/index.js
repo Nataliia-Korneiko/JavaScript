4a348ae2268713caf0645de83d46c4d357feb838
Nataliia-Korneiko <53409659+Nataliia-Korneiko@users.noreply.github.com>
2021-04-04 19:53:01 +0300

    added js-12.1 - pagination

--- js-12.1/src/index.js
+++ js-12.1/src/index.js
@@ -1,18 +1,40 @@
-import './sass/styles.scss';
-import './js/base';
-import fetchArticles from './js/fetch-articles';
+import newsService from './js/news-service';
 import updateArticlesMarkup from './js/update-articles-markup';
+import LoadMoreBtn from './js/components/load-more-button';
 import refs from './js/refs';
+import './js/base';
+import './sass/styles.scss';
 
-refs.searchForm.addEventListener('submit', event => {
+const loadMoreBtn = new LoadMoreBtn({
+  selector: 'button[data-action="load-more"]',
+  hidden: true,
+});
+
+refs.searchForm.addEventListener('submit', searchFormSubmitHandler);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+
+function searchFormSubmitHandler(event) {
   event.preventDefault();
 
   const form = event.currentTarget;
-  const inputValue = form.elements.query.value;
-  // console.log(inputValue);
+  newsService.query = form.elements.query.value;
 
-  refs.articlesContainer.innerHTML = '';
+  clearArticlesContainer();
+  newsService.resetPage();
+  fetchArticles();
   form.reset();
+}
 
-  fetchArticles(inputValue).then(updateArticlesMarkup); // колбек
-});
+function fetchArticles() {
+  loadMoreBtn.disable();
+
+  newsService.fetchArticles().then(articles => {
+    updateArticlesMarkup(articles);
+    loadMoreBtn.show();
+    loadMoreBtn.enable();
+  });
+}
+
+function clearArticlesContainer() {
+  refs.articlesContainer.innerHTML = ''; // убиваем разметку при смене запроса
+}
--- js-12.1/src/js/update-articles-markup.js
+++ js-12.1/src/js/update-articles-markup.js
@@ -5,7 +5,7 @@ function updateArticlesMarkup(articles) {
   const markup = articlesTpl(articles);
   // console.log(markup);
 
-  refs.articlesContainer.insertAdjacentHTML('beforeend', markup);
+  refs.articlesContainer.insertAdjacentHTML('beforeend', markup); // Используем insertAdjacentHTML, он добавляет новую разметку к старой, а не перерисовывает все заново
 }
 
 export default updateArticlesMarkup;
